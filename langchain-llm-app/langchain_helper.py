efe414184c910ba941fd0fd29a5de0d4e92e11c5
harishgandhi-1509 <harishgandhi1509@gmail.com>
2026-02-26 00:29:18 +0530

    Implement real time search with langchain tool

--- langchain-llm-app/langchain_helper.py
+++ langchain-llm-app/langchain_helper.py
@@ -1,6 +1,11 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.agents import create_agent
+
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,6 +22,37 @@ def generate_pet_names(animal_type: str, pet_color: str = "brown") -> str:
     return name_chain.invoke({"animal_type": animal_type, "pet_color": pet_color})
 
 
+def langchain_search_agent():
+
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        # temperature=0.5,
+    )
+
+    search_tool = DuckDuckGoSearchRun()
+
+    agent = create_agent(
+        llm,
+        [search_tool],
+        system_prompt="""
+        You are a helpful assistant.
+        Use the DuckDuckGoSearchRun tool whenever the question
+        requires real-time or factual web information.
+        """,
+    )
+
+    while True:
+        user_query = input("\nAsk something (type 'exit' to quit): ")
+
+        if user_query.lower() == "exit":
+            break
+
+        result = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
+
+        print("\n🤖", result["messages"][-1].content)
+
+
 if __name__ == "__main__":
-    pet_names = generate_pet_names("dog")
-    print(pet_names)
+    # pet_names = generate_pet_names("dog")
+    # print(pet_names)
+    langchain_search_agent()
