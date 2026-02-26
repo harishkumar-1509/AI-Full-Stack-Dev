8d28e5955e93e43748402937d397bb00d8dcc02e
harishgandhi-1509 <harishgandhi1509@gmail.com>
2026-02-26 23:38:51 +0530

    Implement youtube subscript parser langchain

--- langchain-llm-app/langchain_helper.py
+++ langchain-llm-app/langchain_helper.py
@@ -1,10 +1,19 @@
-from langchain_openai import ChatOpenAI
+import faiss
+
+from openai import OpenAI
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 from langchain_community.tools import DuckDuckGoSearchRun
-from langchain.agents import create_agent
+from langchain_community.document_loaders import YoutubeLoader
+from langchain_community.vectorstores import FAISS
 
+from langchain.agents import create_agent
 
 from dotenv import load_dotenv
 
@@ -52,7 +61,70 @@ def langchain_search_agent():
         print("\n🤖", result["messages"][-1].content)
 
 
+embeddings = OpenAIEmbeddings()
+
+video_url = "https://www.youtube.com/watch?v=5MWT_doo68k"
+
+
+def create_vector_db_from_youtube(video_url: str) -> FAISS:
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+
+    docs = text_splitter.split_documents(transcript)
+    # print(docs, "\n")
+
+    db = FAISS.from_documents(
+        docs,
+        embeddings,
+    )
+    return db
+
+
+def get_response_from_query(db, query, k=4):
+    # text-davinci can handle 4097 tokens
+    docs = db.similarity_search(query, k=k)
+    docs_page_content = " ".join([d.page_content for d in docs])
+
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        # temperature=0.5,
+    )
+    prompt = PromptTemplate(
+        input_variables=["question", "docs"],
+        template="""
+        You are a helpful youtube assistant that can answer questions about videos based on video transcript.
+
+        Answer the following question: {question}
+        By searching the following video transcript: {docs}
+
+        Only use the factual information from the transcript to answer the question
+
+        If you fell like you don't have enough information, say "I don't know"
+
+        Your answer should be detailed.
+        """,
+    )
+
+    chain = prompt | llm | StrOutputParser()
+    return chain.invoke({"question": query, "docs": docs_page_content})
+
+
 if __name__ == "__main__":
     # pet_names = generate_pet_names("dog")
     # print(pet_names)
-    langchain_search_agent()
+
+    # langchain_search_agent()
+
+    query = input("Enter some question about that video:")
+    db = create_vector_db_from_youtube(video_url)
+    print(
+        get_response_from_query(
+            db=db,
+            query=query,
+        )
+    )
